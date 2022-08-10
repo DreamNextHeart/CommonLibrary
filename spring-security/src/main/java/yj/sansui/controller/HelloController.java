102371cfe0f8fef05305c456a4a4a929a0d12aa0
PoisonSansui <15767686505@163.com>
2022-08-11 00:10:18 +0800

    集成SpringSecurity

---
+++ spring-security/src/main/java/yj/sansui/controller/HelloController.java
@@ -0,0 +1,16 @@
+package yj.sansui.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sansui
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return  "hello";
+    }
+}
