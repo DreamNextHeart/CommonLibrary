b8c0ba8a1515f740ca9890078804ec67792a0ee3
PoisonSansui <15767686505@163.com>
2022-08-15 19:34:50 +0800

    新增验证码

--- spring-security/src/main/java/yj/sansui/controller/HelloController.java
+++ spring-security/src/main/java/yj/sansui/controller/HelloController.java
@@ -3,6 +3,13 @@ package yj.sansui.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import yj.sansui.utils.VerifyCodeImage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * @author sansui
@@ -12,5 +19,15 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return  "hello";
+    }
+
+    @GetMapping("/vercode")
+    public void code(HttpServletRequest req, HttpServletResponse resp)throws IOException{
+        VerifyCodeImage vc=new VerifyCodeImage();
+        BufferedImage image= vc.getImage();
+        String text= vc.getText();
+        HttpSession session= req.getSession();
+        session.setAttribute("index_code",text);
+        VerifyCodeImage.output(image,resp.getOutputStream());
     }
 }
