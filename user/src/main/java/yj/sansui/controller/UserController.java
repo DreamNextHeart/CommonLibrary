9561195b717635b167ade3303c9d96b6fffacc45
PoisonSansui <15767686505@163.com>
2022-09-25 23:52:55 +0800

    中间版本

--- user/src/main/java/yj/sansui/controller/UserController.java
+++ user/src/main/java/yj/sansui/controller/UserController.java
@@ -1,6 +1,7 @@
 package yj.sansui.controller;
 
 import cn.dev33.satoken.session.SaSession;
+import cn.dev33.satoken.stp.StpUtil;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.web.bind.annotation.*;
 import yj.sansui.bean.dto.UserDTO;
@@ -45,6 +46,11 @@ public class UserController {
     @PostMapping("/register")
     public Result register(User user){
         return userService.register(user);
+    }
+
+    @GetMapping("/logout")
+    public void logout(){
+        StpUtil.logout(1);
     }
 
 }
