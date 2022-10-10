8adc42b3cb0357ae669a5efe2172a5b47feb46e4
PoisonSansui <15767686505@163.com>
2022-10-10 19:52:59 +0800

    树状菜单导航栏

--- user/src/main/java/yj/sansui/controller/MenuController.java
+++ user/src/main/java/yj/sansui/controller/MenuController.java
@@ -3,13 +3,7 @@ package yj.sansui.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yj.sansui.bean.entity.Menu;
-import yj.sansui.bean.entity.Role;
-import yj.sansui.bean.entity.User;
-import yj.sansui.mapper.MenuMapper;
-import yj.sansui.mapper.RoleMapper;
-import yj.sansui.mapper.UserMapper;
 import yj.sansui.service.MenuService;
-
 import javax.annotation.Resource;
 import java.util.List;
 
