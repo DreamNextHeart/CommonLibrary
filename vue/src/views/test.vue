bc47e4718ab6de88db3614a56cba5256a97f0610
PoisonSansui <15767686505@163.com>
2022-10-17 23:57:03 +0800

    权限认证、动态路由中间版本

--- vue/src/views/test.vue
+++ vue/src/views/test.vue
@@ -1,17 +1,17 @@
 <template>
   <el-container class="layout-container-demo" style="height: 500px">
     <el-aside width="200px">
+<!--el-scrollbar,横向滚动条-->
       <el-scrollbar>
         <el-menu :default-openeds="['1', '3']">
           <el-sub-menu index="1">
             <template #title>
-              <el-icon><message /></el-icon>Navigator One
+              Navigator One
             </template>
-            <el-menu-item-group>
-              <template #title>Group 1</template>
+
               <el-menu-item index="1-1">Option 1</el-menu-item>
               <el-menu-item index="1-2">Option 2</el-menu-item>
-            </el-menu-item-group>
+
             <el-menu-item-group title="Group 2">
               <el-menu-item index="1-3">Option 3</el-menu-item>
             </el-menu-item-group>
---
+++ vue/src/views/test1.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container style="height: 500px">
+    <el-aside width="200px">
+      <el-scrollbar>
+        <el-menu>
+          <el-sub-menu index="1">
+            <template #title>第一个导航栏</template>
+              <el-menu-item index="1-1">Option 1</el-menu-item>
+              <el-menu-item index="1-2">Option 2</el-menu-item>
+            <el-sub-menu index="1-3">
+              <template #title>Option 3</template>
+              <el-menu-item index="1-3-1">Option 3-1</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
+    </el-aside>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "test1"
+}
+</script>
+
+<style scoped>
+
+</style>
